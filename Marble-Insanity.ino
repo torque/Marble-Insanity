cc02acc0ed3c93b9964f27ff3023576c190fedcf
torque <torque@1>
2015-12-01 22:34:43 -0800

    Marble-Insanity: implement gameplay routine.

--- Marble-Insanity.ino
+++ Marble-Insanity.ino
@@ -1,24 +1,27 @@
 #include "Nunchuck.hpp"
 #include "AttitudeControl.hpp"
 #include "LoopTime.hpp"
+#include "Buzzer.hpp"
+#include "Timer.hpp"
+
 #define sleep delay
 #define usleep delayMicroseconds
 // #include "Buzzer.hpp"
 // #include "Timer.hpp"
 
 Nunchuck *nunchuck;
-// Buzzer *buzzer;
-// Timer *timer;
+Buzzer *buzzer;
+Timer *timer;
 AttitudeControl *servos;
 
 void setup( void ) {
 	Serial.begin( 115200 );
 	nunchuck = new Nunchuck( );
-	nunchuck->calibrate( );
 	// buzzer = new Buzzer( );
-	// timer = new Timer( buzzer );
 	timer = new Timer( buzzer );
 	servos = new AttitudeControl( nunchuck );
+	servos->reset( );
+	nunchuck->calibrate( );
 }
 
 void loop( void ) {
@@ -26,15 +29,16 @@ void loop( void ) {
 
 	// gameplay routine
 	LoopTime::update( );
-	// timer->startCountdown( );
-	// while ( timer->update( ) ) {
+	timer->startCountdown( );
+	while ( timer->update( ) ) {
 		// read joystick inputs, write servos, check victory condition
 		nunchuck->updateJoystick( );
 		servos->update( );
 		// Serial.println( "pitch: " + String( nunchuck->pitchDegrees( ) ) + ", roll: " + String( nunchuck->rollDegrees( ) ) );
-		sleep( 1 );
-	// }
+		sleep( 5 );
 		LoopTime::update( );
+	}
+
 	servos->reset( );
 	// game over routine
 }
