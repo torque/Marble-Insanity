9c950f808b89b383647a49e1d53b2cf32ef6159f
torque <torque@1>
2015-12-01 18:59:18 -0800

    Marble-Insanity: modifications for testing.

--- Marble-Insanity.ino
+++ Marble-Insanity.ino
@@ -1,13 +1,25 @@
+#include "Nunchuck.hpp"
+#include "AttitudeControl.hpp"
 #include "LoopTime.hpp"
-#include "Buzzer.hpp"
-#include "Timer.hpp"
+#define sleep delay
+#define usleep delayMicroseconds
+// #include "Buzzer.hpp"
+// #include "Timer.hpp"
 
-Buzzer *buzzer;
-Timer *timer;
+Nunchuck *nunchuck;
+// Buzzer *buzzer;
+// Timer *timer;
+AttitudeControl *pitch;
+AttitudeControl *roll;
 
 void setup( void ) {
-	buzzer = new Buzzer( );
-	timer = new Timer( buzzer );
+	Serial.begin( 115200 );
+	nunchuck = new Nunchuck( );
+	nunchuck->calibrate( );
+	// buzzer = new Buzzer( );
+	// timer = new Timer( buzzer );
+	pitch = new AttitudeControl( Pitch, nunchuck );
+	roll = new AttitudeControl( Roll, nunchuck );
 }
 
 void loop( void ) {
@@ -15,10 +27,22 @@ void loop( void ) {
 
 	// gameplay routine
 	LoopTime::update( );
-	timer->startCountdown( );
-	while ( timer->update( ) ) {
+	// timer->startCountdown( );
+	// while ( timer->update( ) ) {
 		// read joystick inputs, write servos, check victory condition
+		nunchuck->updateJoystick( );
+		pitch->update( );
+		roll->update( );
 		LoopTime::update( );
-	}
+		// Serial.println( "pitch: " + String( nunchuck->pitchDegrees( ) ) + ", roll: " + String( nunchuck->rollDegrees( ) ) );
+		sleep( 1 );
+	// }
 	// game over routine
 }
+
+// void loop( void ) {
+// 	nunchuck->updateJoystick( );
+// 	// Serial.println( "x: " + String( nunchuck->joyX ) + ", y: " + String( nunchuck->joyY ) );
+// 	// Serial.println( "pitch: " + String( nunchuck->pitchDegrees( ) ) + ", roll: " + String( nunchuck->rollDegrees( ) ) );
+// 	sleep(500);
+// }
