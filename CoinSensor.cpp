0dfe291dc1c0af4e298df97f9def539e1151e083
torque <torque@1>
2015-12-02 21:43:40 -0800

    CoinSensor: display ADD COIN while idling.

--- CoinSensor.cpp
+++ CoinSensor.cpp
@@ -8,12 +8,22 @@
 #define CoinThreshold 75
 
 void CoinSensor::waitForCoin( void ) {
+	bool toggle = true;
+	char add[4] = { 'A', 'D', 'D', ' ' };
 	char coin[4] = { 'C', 'O', 'I', 'N' };
-	Timer::timer->displayCharacters( coin );
+	Timer::timer->displayCharacters( add );
+	unsigned long now = LoopTime::time;
 	for ( ;; ) {
 		LoopTime::update( );
+		if ( LoopTime::time - now > 1000 ) {
+			now = LoopTime::time;
+			toggle = !toggle;
+			if ( toggle )
+				Timer::timer->displayCharacters( add );
+			else
+				Timer::timer->displayCharacters( coin );
+		}
 		int coinReading = analogRead( CoinSensorPin );
-		Serial.println( "value: " + String( coinReading ) );
 		if ( coinReading < CoinThreshold )
 			break;
 	}
