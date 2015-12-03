ef729a0014a13b79b81623ec4aab8649c37e66f7
torque <torque@1>
2015-12-02 21:42:47 -0800

    alphabet: add numeric cases.

--- alphabet.h
+++ alphabet.h
@@ -69,15 +69,25 @@ static inline uint8_t charToDisplay( unsigned char letter ) {
 		// case 'X':
 		case 'Y': return 0b00111011;
 		case 'Z': return 0b01101101;
+		case 0:
 		case '0': return 0b01111110;
+		case 1:
 		case '1': return 0b00110000;
+		case 2:
 		case '2': return 0b01101101;
+		case 3:
 		case '3': return 0b01111001;
+		case 4:
 		case '4': return 0b00110011;
+		case 5:
 		case '5': return 0b01011011;
+		case 6:
 		case '6': return 0b01011111;
+		case 7:
 		case '7': return 0b01110000;
+		case 8:
 		case '8': return 0b01111111;
+		case 9:
 		case '9': return 0b01111011;
 		default:	return 0b00000000;
 	}
